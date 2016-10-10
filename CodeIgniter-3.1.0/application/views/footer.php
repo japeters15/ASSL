e300f5f19e53f29dc88e61bbcc22bbc999800791
Janay Peters <japeters15@gmail.com>
2016-10-09 21:05:20 -0400

    PorjectVersion1
    Didn’t do so well this week! I have watched multiple tutorials and
    still had no hope! Please let me know where I went wrong in my code, I
    will be buckling down this week to get these features working as well
    as the version 2 features working! Thank you

---
+++ CodeIgniter-3.1.0/application/views/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="<?php echo base_url();?>js/home.js" type="text/javascript"></script>
+</head>
+<body>
+    
+    
+    
+</body>
+</html>
